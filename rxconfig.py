480e1e935f5d34e1cdf941e14a4ed44423a25bbf
Andreas Mueller <andreasmu@yahoo.com>
2026-01-26 17:44:15 +0100

    Set backend URL for Render

--- rxconfig.py
+++ rxconfig.py
@@ -1,9 +1,17 @@
 import reflex as rx
-
-import reflex as rx
+import os
 
 config = rx.Config(
-    app_name="golf_reflex",
-    backend_url="https://scores-s0e0.onrender.com",
+    app_name="golf_reflex",  # Must match your app class name
+    api_url=os.environ.get("RENDER_EXTERNAL_URL", ""),  # Critical!
+    deploy_url=os.environ.get("RENDER_EXTERNAL_URL", ""),
+    cors_allowed_origins=[
+        os.environ.get("RENDER_EXTERNAL_URL", ""),
+        "http://localhost:3000",
+        "http://localhost:8000",
+    ],
+    backend_port=8000,
+    frontend_port=3000,
+    # Enable these for production
+    env=rx.Env.PROD,
 )
-
